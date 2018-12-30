3fc0203ec122e335aeab4314b5f684492d597dac
benleahy91 <benleahy91@gmail.com>
2018-12-30 11:07:19 -0500

    Mongo Add Product

--- models/user.js
+++ models/user.js
@@ -4,15 +4,30 @@ const getDb = require('../util/database').getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
-  };
-
+    this.cart = cart; // {items: []}
+    this._id = id;
+  }
   save() {
     const db = getDb();
     return db.collection('users').insertOne(this);
-  };
+	};
+	
+	addToCart(product) {
+		// const cartProduct = this.cart.items.findIndex(cp => {
+		// 	return cp._id === product._id;
+		// });
+		const updatedCart = [{productId: new ObjectId(product._id), quantity: 1}];
+		const db = getDb();
+		return db
+		.collection('users')
+		.updateOne(
+			{ _id: new ObjectId(this._id) },
+			{ $set: {cart: updatedCart} }
+		);
+	};
 
   static findById(userId) {
     const db = getDb();
@@ -20,8 +35,8 @@ class User {
       .collection('users')
       .findOne({ _id: new ObjectId(userId) })
       .then(user => {
-				console.log(user);
-				console.log(userId);
+				// console.log(user);
+				// console.log(userId);
         return user;
       })
       .catch(err => {
