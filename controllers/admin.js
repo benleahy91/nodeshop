9d7858fc16336978cf9edb7c12fab52536ed25b5
benleahy91 <benleahy91@gmail.com>
2018-12-31 14:54:13 -0500

    set up connnection and storing to database

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,85 +10,85 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageURL = req.body.imageURL;
+  const imageUrl = req.body.imageURL;
   const price = req.body.price;
 	const description = req.body.description;
-	const product = new Product(
-		title,
-		price,
-		description,
-		imageURL,
-		null,
-		req.user._id
-	);
-	
-	product.save()
+	const product = new Product({
+		title: title,
+		price: price,
+		description: description,
+		imageUrl: imageUrl
+	});
+	product
+	.save()
 	.then(result => {
+		console.log('Product Created');
+		
 		res.redirect('/admin/products')
 	})
 	.catch(err => {console.log(err)});
 };
 
-exports.getProducts = (req, res, next) => {
-	Product.fetchAll()
-	.then(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-	})
-	.catch(err => {console.log(err)});
-};
+// exports.getProducts = (req, res, next) => {
+// 	Product.fetchAll()
+// 	.then(products => {
+//     res.render('admin/products', {
+//       prods: products,
+//       pageTitle: 'Admin Products',
+//       path: '/admin/products'
+//     });
+// 	})
+// 	.catch(err => {console.log(err)});
+// };
 
-exports.getEditProduct = (req, res, next) => {
-  const editMode = req.query.edit;
-  if (!editMode) {
-    return res.redirect('/');
-  };
-	const prodId = req.params.prodId;
-	Product.findById(prodId)
-	.then(product => {
-		if (!product) {
-			return res.redirect('/')
-		}
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
-    });
-	})
-	.catch(err => {console.log(err)});
-};
+// exports.getEditProduct = (req, res, next) => {
+//   const editMode = req.query.edit;
+//   if (!editMode) {
+//     return res.redirect('/');
+//   };
+// 	const prodId = req.params.prodId;
+// 	Product.findById(prodId)
+// 	.then(product => {
+// 		if (!product) {
+// 			return res.redirect('/')
+// 		}
+//     res.render('admin/edit-product', {
+//       pageTitle: 'Edit Product',
+//       path: '/admin/edit-product',
+//       editing: editMode,
+//       product: product
+//     });
+// 	})
+// 	.catch(err => {console.log(err)});
+// };
 
-exports.postEditProduct = (req, res, next) => {
-	const prodId = req.body.productId;
-	const updatedTitle = req.body.title;
-	const updatedImageURL = req.body.imageURL;
-	const updatedPrice = req.body.price;
-	const updatedDesc = req.body.description;
+// exports.postEditProduct = (req, res, next) => {
+// 	const prodId = req.body.productId;
+// 	const updatedTitle = req.body.title;
+// 	const updatedImageURL = req.body.imageURL;
+// 	const updatedPrice = req.body.price;
+// 	const updatedDesc = req.body.description;
 
-	Product.findById(prodId)
-	const product = new Product(
-		updatedTitle, updatedPrice,
-		updatedDesc,
-		updatedImageURL,
-		prodId
-	);
-	product
-	.save()
-	.then(result => {
-		res.redirect('/admin/products');
-	})
-	.catch(err => {console.log(err)});
-};
-exports.postDeleteProduct = (req, res, next) => {
-	const prodId = req.body.productId;	
-	Product.deleteById(prodId)
-	.then(result => {
-		res.redirect('/admin/products');
+// 	Product.findById(prodId)
+// 	const product = new Product(
+// 		updatedTitle, updatedPrice,
+// 		updatedDesc,
+// 		updatedImageURL,
+// 		prodId
+// 	);
+// 	product
+// 	.save()
+// 	.then(result => {
+// 		res.redirect('/admin/products');
+// 	})
+// 	.catch(err => {console.log(err)});
+// };
+// exports.postDeleteProduct = (req, res, next) => {
+// 	const prodId = req.body.productId;	
+// 	Product.deleteById(prodId)
+// 	.then(result => {
+// 		res.redirect('/admin/products');
 		
-	})
-	.catch(err => {console.log(err)});
-};
+// 	})
+// 	.catch(err => {console.log(err)});
+// };
