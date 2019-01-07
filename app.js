40660a6beff65fa01c6381b151c3b7c3efbe6a88
benleahy91 <benleahy91@gmail.com>
2019-01-07 12:49:49 -0500

    finished sessions and cookies

--- app.js
+++ app.js
@@ -37,7 +37,10 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  User.findById('5c2bc6ac0778a152cdc8fa14')
+	if (!req.session.user) {
+		return next();
+	}
+  User.findById(req.session.user._id)
 	.then(user => {
 		req.user = user;
 		next();
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,6 @@
-exports.getLogin = (req, res, next) => {
-	// const isLoggedIn =req
-	// .get('Cookie')
-	// .trim()
-	// .split('=')[1] === 'true';
+const User = require('../models/user');
 
+exports.getLogin = (req, res, next) => {
 	res.render('auth/login', {
 		path: '/login',
 		pageTitle: 'Login',
@@ -11,12 +8,21 @@ exports.getLogin = (req, res, next) => {
 	});		
 };
 
-// exports.postLogin = (req, res, next) => {
-// 	res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
-// 	res.redirect('/')
-// };
-
 exports.postLogin = (req, res, next) => {
-	req.session.isLoggedIn = true;
-	res.redirect('/')
+	User.findById('5c2bc6ac0778a152cdc8fa14')
+	.then(user => {
+		req.session.isLoggedIn = true;
+		req.session.user= user;
+		req.session.save((err) => {
+			console.log(err);
+			res.redirect('/')
+		});
+	});
+};
+
+exports.postLogout = (req, res, next) => {
+	req.session.destroy(err => {
+		console.log(err);
+		res.redirect('/')
+	});
 };
